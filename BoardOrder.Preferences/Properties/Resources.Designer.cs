c95a7f492014611ff712f3360aef1ee5eec22558
Jakub Swietek <jakub.swietek@gmail.com>
2020-11-01 11:12:35 +0100

    Added basic resources

--- BoardOrder.Preferences/Properties/Resources.Designer.cs
+++ BoardOrder.Preferences/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace BoardOrder.Preferences.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace BoardOrder.Preferences.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("BoardOrder.Preferences.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace BoardOrder.Preferences.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -61,45 +61,162 @@ namespace BoardOrder.Preferences.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to .
+        ///   Looks up a localized string similar to Board Thickness.
         /// </summary>
-        internal static string BOARD_THICKNESS_LABEL {
+        public static string BOARD_THICKNESS_LABEL {
             get {
                 return ResourceManager.GetString("BOARD_THICKNESS_LABEL", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to .
+        ///   Looks up a localized string similar to Boards Quantity.
         /// </summary>
-        internal static string BOARDS_QUANTITY_LABEL {
+        public static string BOARDS_QUANTITY_LABEL {
             get {
                 return ResourceManager.GetString("BOARDS_QUANTITY_LABEL", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to .
+        ///   Looks up a localized string similar to Controlled Impedance.
         /// </summary>
-        internal static string MATERIAL_LABEL {
+        public static string CONTROLLED_IMPEDANCE_LABEL {
+            get {
+                return ResourceManager.GetString("CONTROLLED_IMPEDANCE_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Coper Weight on Inner Layers.
+        /// </summary>
+        public static string COOPER_WEIGHT_ON_INNER_LAYERS_LABEL {
+            get {
+                return ResourceManager.GetString("COOPER_WEIGHT_ON_INNER_LAYERS_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Coper Weight on Outer Layers.
+        /// </summary>
+        public static string COOPER_WEIGHT_ON_OUTER_LAYERS_LABEL {
+            get {
+                return ResourceManager.GetString("COOPER_WEIGHT_ON_OUTER_LAYERS_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Flux Type.
+        /// </summary>
+        public static string FLUX_TYPE_LABEL {
+            get {
+                return ResourceManager.GetString("FLUX_TYPE_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to IPC Class.
+        /// </summary>
+        public static string IPC_CLASS_LABEL {
+            get {
+                return ResourceManager.GetString("IPC_CLASS_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ITAR.
+        /// </summary>
+        public static string ITAR_LABEL {
+            get {
+                return ResourceManager.GetString("ITAR_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Lead Free.
+        /// </summary>
+        public static string LEAD_FREE_LABEL {
+            get {
+                return ResourceManager.GetString("LEAD_FREE_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Material.
+        /// </summary>
+        public static string MATERIAL_LABEL {
             get {
                 return ResourceManager.GetString("MATERIAL_LABEL", resourceCulture);
             }
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Notes.
+        /// </summary>
+        public static string NOTES_LABEL {
+            get {
+                return ResourceManager.GetString("NOTES_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Project Name.
         /// </summary>
-        internal static string PROJECT_NAME_LABEL {
+        public static string PROJECT_NAME_LABEL {
             get {
                 return ResourceManager.GetString("PROJECT_NAME_LABEL", resourceCulture);
             }
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Silkscreen Color.
+        /// </summary>
+        public static string SILKSCREEN_COLOR_LABEL {
+            get {
+                return ResourceManager.GetString("SILKSCREEN_COLOR_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Solder Mask Color.
+        /// </summary>
+        public static string SOLDER_MASK_COLOR_LABEL {
+            get {
+                return ResourceManager.GetString("SOLDER_MASK_COLOR_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Stackup.
+        /// </summary>
+        public static string STACKUP_LABEL {
+            get {
+                return ResourceManager.GetString("STACKUP_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Surface Finish.
+        /// </summary>
+        public static string SURFACE_FINISH_LABEL {
+            get {
+                return ResourceManager.GetString("SURFACE_FINISH_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Tenting for Vias.
+        /// </summary>
+        public static string TENTING_FOR_VIAS_LABEL {
+            get {
+                return ResourceManager.GetString("TENTING_FOR_VIAS_LABEL", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Zipcode.
         /// </summary>
-        internal static string ZIPCODE_LABEL {
+        public static string ZIPCODE_LABEL {
             get {
                 return ResourceManager.GetString("ZIPCODE_LABEL", resourceCulture);
             }
--- BoardOrder.Preferences/Views/PreferencesView.xaml.cs
+++ BoardOrder.Preferences/Views/PreferencesView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BoardOrder.Preferences {
+namespace BoardOrder.Preferences.Views {
 	/// <summary>
 	/// Interaction logic for PreferencesView.xaml
 	/// </summary>
