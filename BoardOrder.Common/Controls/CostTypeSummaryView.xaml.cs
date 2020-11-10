4caa7916227e96e6de97789507105357ce264ac5
Jakub Swietek <jakub.swietek@gmail.com>
2020-11-10 16:11:11 +0100

    Sorting

--- BoardOrder.Common/Controls/CostTypeSummaryView.xaml.cs
+++ BoardOrder.Common/Controls/CostTypeSummaryView.xaml.cs
@@ -8,13 +8,14 @@ using System.ComponentModel;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 
 namespace BoardOrder.Common.Controls {
 	/// <summary>
 	/// Interaction logic for CostTypeSummaryView.xaml
 	/// </summary>
 	public partial class CostTypeSummaryView : UserControl, INotifyPropertyChanged {
-		private ObservableCollection<ICostSummaryItem> filteredItemsSource;
+		private CollectionViewSource filteredItemsSource;
 
 		public CostTypeSummaryView() {
 			InitializeComponent();
@@ -40,70 +41,56 @@ namespace BoardOrder.Common.Controls {
 		public static readonly DependencyProperty BoardsQuantityProperty =
 			DependencyProperty.Register("BoardsQuantity", typeof(int), typeof(CostTypeSummaryView), new PropertyMetadata(0));
 
-		public IEnumerable<ICostSummaryItem> ItemsSource {
-			get { return (IEnumerable<ICostSummaryItem>)GetValue(ItemsSourceProperty); }
+		public ICollectionView ItemsSource {
+			get { return (ICollectionView)GetValue(ItemsSourceProperty); }
 			set { SetValue(ItemsSourceProperty, value); }
 		}
 
 		// Using a DependencyProperty as the backing store for ItemsSource.  This enables animation, styling, binding, etc...
 		public static readonly DependencyProperty ItemsSourceProperty =
-			DependencyProperty.Register("ItemsSource", typeof(IEnumerable<ICostSummaryItem>), typeof(CostTypeSummaryView), new PropertyMetadata(null, ItemsSourceChanged));
+			DependencyProperty.Register("ItemsSource", typeof(ICollectionView), typeof(CostTypeSummaryView), new PropertyMetadata(null));
 
-		public CostType CostTypeFilter {
-			get { return (CostType)GetValue(CostTypeFilterProperty); }
-			set { SetValue(CostTypeFilterProperty, value); }
-		}
 
-		// Using a DependencyProperty as the backing store for CostType.  This enables animation, styling, binding, etc...
-		public static readonly DependencyProperty CostTypeFilterProperty =
-			DependencyProperty.Register("CostTypeFilter", typeof(CostType), typeof(CostTypeSummaryView), new PropertyMetadata(CostType.Assembly, CostTypeFilterChanged));
+		//private static void ItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
 
-		private static void CostTypeFilterChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
-			if (d is CostTypeSummaryView summaryView) {
-				summaryView.FilteredItemsSource = summaryView.ItemsSource != null ? new ObservableCollection<ICostSummaryItem>(summaryView.ItemsSource.Where(item => item.Item.CostType == summaryView.CostTypeFilter)) : null;
-			}
-		}
+		//	if (d is CostTypeSummaryView summaryView) {
+		//		if (e.OldValue is INotifyCollectionChanged oldSource) {
+		//			//oldSource.CollectionChanged -= summaryView.SourceChanged;
+		//		}
+		//		if (e.NewValue is INotifyCollectionChanged newSource) {
+		//			//newSource.CollectionChanged += summaryView.SourceChanged;
+		//		}
+		//		summaryView.FilteredItemsSource.Source = e.NewValue;// is IEnumerable<ICostSummaryItem> source ? new ObservableCollection<ICostSummaryItem>(source.Where(item => item.Item.CostType == summaryView.CostTypeFilter)) : null;
+		//	}
+		//}
 
-		private static void ItemsSourceChanged(DependencyObject d, DependencyPropertyChangedEventArgs e) {
+		//private void SourceChanged(object sender, NotifyCollectionChangedEventArgs e) {
+		//	var oldItem = e.OldItems[0] as ICostSummaryItem;
+		//	var newItem = e.NewItems[0] as ICostSummaryItem;
+		//	switch (e.Action) {
+		//		case NotifyCollectionChangedAction.Add:
+		//			if (newItem?.Item.CostType == this.CostTypeFilter) {
+		//				this.FilteredItemsSource.Add(newItem);
+		//			}
+		//			break;
+		//		case NotifyCollectionChangedAction.Remove:
+		//			if (oldItem?.Item.CostType == this.CostTypeFilter) {
+		//				this.FilteredItemsSource.Remove(oldItem);
+		//			}
+		//			break;
+		//		case NotifyCollectionChangedAction.Replace:
+		//			if (oldItem.Item.CostType == this.CostTypeFilter) {
+		//				var index = this.FilteredItemsSource.IndexOf(oldItem);
+		//				this.FilteredItemsSource[index] = newItem;
+		//			}
+		//			break;
+		//		case NotifyCollectionChangedAction.Reset:
+		//			this.FilteredItemsSource.Source = new ObservableCollection<ICostSummaryItem>(this.ItemsSource.Where(item => item.Item.CostType == this.CostTypeFilter));
+		//			break;
+		//	}
+		//}
 
-			if (d is CostTypeSummaryView summaryView) {
-				if (e.OldValue is INotifyCollectionChanged oldSource) {
-					oldSource.CollectionChanged -= summaryView.SourceChanged;
-				}
-				if (e.NewValue is INotifyCollectionChanged newSource) {
-					newSource.CollectionChanged += summaryView.SourceChanged;
-				}
-				summaryView.FilteredItemsSource = e.NewValue is IEnumerable<ICostSummaryItem> source ? new ObservableCollection<ICostSummaryItem>(source.Where(item => item.Item.CostType == summaryView.CostTypeFilter)) : null;
-			}
-		}
-
-		private void SourceChanged(object sender, NotifyCollectionChangedEventArgs e) {
-			var oldItem = e.OldItems[0] as ICostSummaryItem;
-			var newItem = e.NewItems[0] as ICostSummaryItem;
-			switch (e.Action) {
-				case NotifyCollectionChangedAction.Add:
-					if (newItem?.Item.CostType == this.CostTypeFilter) {
-						this.FilteredItemsSource.Add(newItem);
-					}
-					break;
-				case NotifyCollectionChangedAction.Remove:
-					if (oldItem?.Item.CostType == this.CostTypeFilter) {
-						this.FilteredItemsSource.Remove(oldItem);
-					}
-					break;
-				case NotifyCollectionChangedAction.Replace:
-					if (oldItem.Item.CostType == this.CostTypeFilter) {
-						var index = this.FilteredItemsSource.IndexOf(oldItem);
-						this.FilteredItemsSource[index] = newItem;
-					}
-					break;
-				case NotifyCollectionChangedAction.Reset:
-					this.FilteredItemsSource = new ObservableCollection<ICostSummaryItem>(this.ItemsSource.Where(item => item.Item.CostType == this.CostTypeFilter));
-					break;
-			}
-		}
-
-		public ObservableCollection<ICostSummaryItem> FilteredItemsSource {
+		public CollectionViewSource FilteredItemsSource {
 			get => this.filteredItemsSource;
 			private set {
 				this.filteredItemsSource = value;
--- BoardOrder/ViewModel/QuoteViewModel.cs
+++ BoardOrder/ViewModel/QuoteViewModel.cs
@@ -6,13 +6,20 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
+using System.Management.Instrumentation;
+using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace BoardOrder.ViewModel {
 	public class QuoteViewModel : ViewModelBase {
 		private readonly IQuoteManager quoteOrderManager;
 
 		private ObservableCollection<CostsSummaryItemViewModel> costsSummaryItems;
+		private ICollectionView quoteFabricationView;
+		private ICollectionView quoteAssemblyView;
+		private ICollectionView quoteComponentsView;
 		private ObservableCollection<BoardOrderItem> quote;
 		private bool isTimeSummaryVisible;
 		private bool isCostSummaryVisible;
@@ -45,6 +52,7 @@ namespace BoardOrder.ViewModel {
 				this.Set(nameof(this.IsTimeSummaryVisible), ref this.isTimeSummaryVisible, value);
 				this.isCostSummaryVisible = !value;
 				RaisePropertyChanged(nameof(this.IsCostSummaryVisible));
+				SetSorting();
 			}
 		}
 
@@ -54,6 +62,28 @@ namespace BoardOrder.ViewModel {
 				this.Set(nameof(this.IsCostSummaryVisible), ref this.isCostSummaryVisible, value);
 				this.isTimeSummaryVisible = !value;
 				RaisePropertyChanged(nameof(this.IsTimeSummaryVisible));
+				SetSorting();
+			}
+		}
+
+		public ICollectionView QuoteFabricationView {
+			get => this.quoteFabricationView;
+			set {
+				this.Set(nameof(this.QuoteFabricationView), ref this.quoteFabricationView, value);
+			}
+		}
+
+		public ICollectionView QuoteAssemblyView {
+			get => this.quoteAssemblyView;
+			set {
+				this.Set(nameof(this.QuoteAssemblyView), ref this.quoteAssemblyView, value);
+			}
+		}
+
+		public ICollectionView QuoteComponentsView {
+			get => this.quoteComponentsView;
+			set {
+				this.Set(nameof(this.QuoteComponentsView), ref this.quoteComponentsView, value);
 			}
 		}
 
@@ -100,6 +130,11 @@ namespace BoardOrder.ViewModel {
 			this.Quote = value;
 			this.CostsSummaryItems = new ObservableCollection<CostsSummaryItemViewModel>(value.Select(item => CreateSummaryItem(item)));
 			this.Quote.CollectionChanged += this.HandleQuoteChanged;
+			this.QuoteFabricationView = (new CollectionViewSource() { Source = this.CostsSummaryItems }).View;
+			this.QuoteAssemblyView = (new CollectionViewSource() { Source = this.CostsSummaryItems }).View;
+			this.QuoteComponentsView = (new CollectionViewSource() { Source = this.CostsSummaryItems }).View;
+			this.SetSorting();
+			this.SetFilters();
 			this.IsCostSummaryVisible = true;
 			this.RaiseCostsChanged();
 		}
@@ -145,6 +180,52 @@ namespace BoardOrder.ViewModel {
 
 		private double CalculateSumTime(IEnumerable<BoardOrderItem> items) {
 			return Math.Round(items?.Sum(item => item.WorkdaysModifier * this.quoteOrderManager.BoardsQuantity) ?? 0);
+		}
+
+		private void SetSorting() {
+			var fabSortDesc = new SortDescription(IsTimeSummaryVisible ?
+				nameof(CostsSummaryItemViewModel.PercentageTime) : nameof(CostsSummaryItemViewModel.PercentageCost), ListSortDirection.Descending);
+
+			var assemblySortDesc = new SortDescription(IsTimeSummaryVisible ?
+					nameof(CostsSummaryItemViewModel.PercentageTime) : nameof(CostsSummaryItemViewModel.PercentageCost), ListSortDirection.Descending);
+
+			var comSortDesc = new SortDescription(IsTimeSummaryVisible ?
+				nameof(CostsSummaryItemViewModel.PercentageTime) : nameof(CostsSummaryItemViewModel.PercentageCost), ListSortDirection.Descending);
+
+			if (this.QuoteFabricationView.SortDescriptions.Count == 0) {
+				this.QuoteFabricationView.SortDescriptions.Add(fabSortDesc);
+			}
+			else {
+				this.QuoteFabricationView.SortDescriptions[0] = fabSortDesc;
+			}
+
+			if (this.QuoteAssemblyView.SortDescriptions.Count == 0) {
+				this.QuoteAssemblyView.SortDescriptions.Add(assemblySortDesc);
+			}
+			else {
+				this.QuoteAssemblyView.SortDescriptions[0] = assemblySortDesc;
+			}
+
+			if (this.QuoteComponentsView.SortDescriptions.Count == 0) {
+				this.QuoteComponentsView.SortDescriptions.Add(comSortDesc);
+			}
+			else {
+				this.QuoteComponentsView.SortDescriptions[0] = comSortDesc;
+			}
+
+			//this.QuoteFabricationView.Refresh();
+			//this.QuoteAssemblyView.Refresh();
+			//this.QuoteComponentsView.Refresh();
+		}
+
+		private void SetFilters() {
+			this.QuoteFabricationView.Filter = item => item is CostsSummaryItemViewModel summary && summary.Item.CostType == CostType.Fabrication;
+			this.QuoteAssemblyView.Filter = item => item is CostsSummaryItemViewModel summary && summary.Item.CostType == CostType.Assembly;
+			this.QuoteComponentsView.Filter = item => item is CostsSummaryItemViewModel summary && summary.Item.CostType == CostType.Parts;
+
+			this.QuoteFabricationView.Refresh();
+			this.QuoteAssemblyView.Refresh();
+			this.QuoteComponentsView.Refresh();
 		}
 
 		private void RaiseCostsChanged() {
